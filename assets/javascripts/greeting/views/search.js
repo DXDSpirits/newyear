a6e5844c44bf4028a1e2cf8207395c61f49f0744
Ran <lr_naril@163.com>
2016-01-20 11:55:51 +0800

    add list filter in search

--- assets/javascripts/greeting/views/search.js
+++ assets/javascripts/greeting/views/search.js
@@ -7,14 +7,73 @@ var GreetingsCollection = Amour.Model.extend({
 });
 
 App.Pages.Search = new (PageView.extend({
-    events: {},
+    events: {
+        'click li.place-item': 'showItems',
+    },
     initPage: function() {
         this.greetings = new GreetingsCollection();
     },
     leave: function() {},
+    showItems: function() {},
     render: function() {
         this.greetings.fetch({
             reset: true
         });
     }
 }))({el: $('#view-search')});
+
+var options = {
+    valueNames: ['name','id'],
+    item: '<li class="place-item"><h5 class="name"></h5><p class="id hidden"></p></li>'
+};
+
+var placesList = new List('places', options);
+
+App.places.fetch({
+    success: function(model) {
+        $("input#search-input").focus(function() {
+            var fetchModel = _.extend(model);
+            $("span.search-cancel").removeClass('hidden');
+            filterPlaces(fetchModel);
+        });
+    }
+});
+
+function filterPlaces(result) {
+    _.each(result.toJSON(), function(item) {
+        // 填充所有省市（直辖市按省级）
+        if(item.category == "province") {
+            placesList.add({
+                name: item.name,
+                id: item.id
+            });
+        }else if(item.category == "city") {
+            if(_.indexOf(['北京', '天津', '重庆', '上海'], item.name) < 0) {
+                placesList.add({
+                    name: item.name,
+                    id: item.id
+                });
+            }
+        }
+    });
+}
+// function hideList() {
+//     $("#place-list-wrapper").addClass('list-transparent');
+// }
+
+$("span.search-cancel").click(function(e) {
+    e.stopPropagation();
+    $("span.search-cancel").addClass('hidden');
+    $("input#search-input").val('');
+    // $("#place-list-wrapper").addClass('list-transparent');
+    // hideList();
+});
+
+$("input#search-input").bind('input propertychange', function() {
+    var val = $("input#search-input").val();
+    if(val) {
+        $("#place-list-wrapper").removeClass('list-transparent');
+    }else {
+        $("#place-list-wrapper").addClass('list-transparent');
+    }
+});
