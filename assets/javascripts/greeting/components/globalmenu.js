8939f4daf195a445c2cb387368633f3e9bffddb0
Xindong Ding <dxd.spirits@gmail.com>
2016-01-28 03:21:31 +0800

    close globalmenu on navigating

--- assets/javascripts/greeting/components/globalmenu.js
+++ assets/javascripts/greeting/components/globalmenu.js
@@ -2,9 +2,14 @@
 var App = require('../app');
 
 var globalMenu = new (Amour.View.extend({
-    events: {
-        'click .btn-logout': 'logout'
-    },
+    events: {},
     initView: function() {},
     render: function() {}
 }))({el: $('#global-menu')});
+
+App.router.on('route', function(name) {
+    globalMenu.$el.toggleClass('hidden', name == 'prologue');
+    if (globalMenu.$el.hasClass('open')) {
+        globalMenu.$('.lantern').dropdown('toggle');
+    }
+});
