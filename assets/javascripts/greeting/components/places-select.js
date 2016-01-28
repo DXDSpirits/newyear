e17faafbc5f008af94506c7f005c8c91ce4536a4
Xindong Ding <dxd.spirits@gmail.com>
2016-01-28 18:00:52 +0800

    style places-select component

--- assets/javascripts/greeting/components/places-select.js
+++ assets/javascripts/greeting/components/places-select.js
@@ -5,7 +5,35 @@ var places = App.places = new (Amour.Collection.extend({
     url: Amour.APIRoot + 'greetings/place/'
 }))();
 
+function stylePlaceList() {
+    var $view = $('.places-select');
+    $view.each(function() {
+        $(this).html($(this).find('select').map(function() {
+            return $('<div class="select-wrapper"></div>').html(this)[0];
+        }));
+    });
+    $view.find('.select-wrapper').addClass('dropdown')
+         .find('select').addClass('needsclick')
+         .after('<div data-toggle="dropdown"></div>')
+         .after('<div class="dropdown-menu"></div>')
+    $view.find('.select-wrapper').on('show.bs.dropdown', function() {
+        var $select = $(this).find('select');
+        var $options = $select.find('option').map(function() {
+            var value = $(this).attr('value');
+            var text = $(this).text();
+            var $a = $('<a></a>').attr('data-value', value).text(text);
+            return $('<li></li>').html($a)[0];
+        });
+        $(this).find('.dropdown-menu').html($options);
+    }).on('click', '.dropdown-menu > li > a', function() {
+        var $select = $(this).closest('.select-wrapper').find('select');
+        var value = $(this).attr('data-value');
+        $select.val(value).trigger('change');
+    });
+}
+
 function initPlaceList() {
+    stylePlaceList();
     var data = places.toJSON();
     var fillPlaces = function($select, places, defaultValue, childCategory) {
         var $places = _.map(places, function(item) {
@@ -15,16 +43,15 @@ function initPlaceList() {
         $select.html($places).prepend($('<option value=""></option>').text(defaultValue));
     };
     var $view = $('.places-select');
-    $view.find('select').addClass('needsclick');
     $view.find('select[name="province"]').on('change', function() {
         var cities = $(this).find('option:selected').data('children') || [];
-        var $select = $(this).siblings('select[name="city"]');
+        var $select = $(this).closest('.places-select').find('select[name="city"]');
         fillPlaces($select, cities, '市', 'district');
         $select.trigger('change');
     });
     $view.find('select[name="city"]').on('change', function() {
         var districts = $(this).find('option:selected').data('children') || [];
-        var $select = $(this).siblings('select[name="district"]');
+        var $select = $(this).closest('.places-select').find('select[name="district"]');
         fillPlaces($select, districts, '区', null);
     });
     var provinces = _.where(data, {category: 'province'});
