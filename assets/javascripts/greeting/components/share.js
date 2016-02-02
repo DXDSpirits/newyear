75e4657594a1eda87d82cbd6ba0645c52418f12a
Xindong Ding <dxd.spirits@gmail.com>
2016-02-03 02:15:56 +0800

    set weixin share title same with page title

--- assets/javascripts/greeting/components/share.js
+++ assets/javascripts/greeting/components/share.js
@@ -5,7 +5,8 @@ if (window.wx) {
     wx.config(WX_CONFIG);
     var setWxShare = App.setWxShare = function(description) {
         var radius = 1 + (+window.location.query.radius || 0);
-        var title = '【乡音祝福 - 八音盒】';
+        var title = $('.view:not(".view-hidden") .header-navbar .navbar-title').text();
+        title = title ? '【' + title + '】' : '【乡音祝福 - 八音盒】';
         var description = description || '乡音无改，录制你的乡音祝福，送给大家';
         var link = [location.origin, '/?radius=', radius, location.hash].join('');
         var img_url = '';
