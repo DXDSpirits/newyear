7fc46f8d67fe60025852805d7716d8a8c2f9e52b
Xindong Ding <dxd.spirits@gmail.com>
2016-02-04 18:12:13 +0800

    typo

--- assets/javascripts/greeting/components/share.js
+++ assets/javascripts/greeting/components/share.js
@@ -43,7 +43,7 @@ function getPageTitle() {
 }
 
 function getShareText(text) {
-    var desc = '#新年祝福接力# 我是全国第' + shares.count + '个分享乡音祝福的人。'];
+    var desc = '#新年祝福接力# 我是全国第' + shares.count + '个分享乡音祝福的人。';
     desc += text || '乡音无改，录制你的乡音祝福，送给大家';
     return desc;
 }
