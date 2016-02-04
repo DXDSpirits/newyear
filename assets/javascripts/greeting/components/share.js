aec3ecf60cf72f5d648c14931c7d0bac9c2264ad
Xindong Ding <dxd.spirits@gmail.com>
2016-02-04 16:45:48 +0800

    organize script of wx_config

--- assets/javascripts/greeting/components/share.js
+++ assets/javascripts/greeting/components/share.js
@@ -19,31 +19,30 @@ function onMenuShare(title, description, link, img_url) {
     });
 }
 
-if (window.wx) {
-    wx.config(WX_CONFIG);
-    var setWxShare = App.setWxShare = function(description) {
-        var radius = +window.location.query.radius || 0;
-        var title = '【乡音祝福 - 八音盒】';
-        if (App.pageRouter.history.active) {
-            title = App.pageRouter.history.active.$('.header-navbar .navbar-title').text();
-            title = '【' + title + '】';
-        }
-        var description = description || '乡音无改，录制你的乡音祝福，送给大家';
-        var img_url = 'http://up.img.8yinhe.cn/o_1aail0mdr13v4rbmhf5ptqkm2a.jpg';
-        App.user.getUserInfo(function() {
-            var queryStr = Amour.encodeQueryString({
-                radius: radius + 1,
-                relay: App.user.id
-            });
-            var link = [location.origin, '/?', queryStr, location.hash].join('');
-            onMenuShare(title, description, link, img_url);
+var setWxShare = App.setWxShare = function(description) {
+    var radius = +window.location.query.radius || 0;
+    var title = '【乡音祝福 - 八音盒】';
+    if (App.pageRouter.history.active) {
+        title = App.pageRouter.history.active.$('.header-navbar .navbar-title').text();
+        title = '【' + title + '】';
+    }
+    var description = description || '乡音无改，录制你的乡音祝福，送给大家';
+    var img_url = 'http://up.img.8yinhe.cn/o_1aail0mdr13v4rbmhf5ptqkm2a.jpg';
+    App.user.getUserInfo(function() {
+        var queryStr = Amour.encodeQueryString({
+            radius: radius + 1,
+            relay: App.user.id
         });
-    };
-    setWxShare();
-    wx.ready(setWxShare);
-    App.router.on('route', function(name) {
-        if (name != 'index') {
-            setWxShare();
-        }
+        var link = [location.origin, '/?', queryStr, location.hash].join('');
+        onMenuShare(title, description, link, img_url);
     });
-}
+};
+
+setWxShare();
+wx.ready(setWxShare);
+
+App.router.on('route', function(name) {
+    if (name != 'index') {
+        setWxShare();
+    }
+});
