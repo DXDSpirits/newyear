8339280a8949ab241839781efa1af43cb59e9e58
Xindong Ding <dxd.spirits@gmail.com>
2016-02-05 13:27:09 +0800

    delay download webfonts

--- assets/javascripts/greeting/components/loadingscreen.js
+++ assets/javascripts/greeting/components/loadingscreen.js
@@ -9,6 +9,7 @@ var loadingEnd = function() {
 };
 
 var onceImagesLoaded = _.once(function() {
+    downloadFontface();
     if (Amour.imagesLoaded) {
         loadingEnd();
     } else {
@@ -20,3 +21,20 @@ _.delay(onceImagesLoaded, 10000);
 $(window).load(function() {
     _.delay(onceImagesLoaded, 2000);
 });
+
+function downloadFontface() {
+    var style = '@font-face { font-family: "qkbysjt"; font-weight: normal; font-style: normal; src: url("http://cdn.greeting.wedfairy.com/assets/fonts/qkbysjt-full-webfont.eot"); src: url("http://cdn.greeting.wedfairy.com/assets/fonts/qkbysjt-full-webfont.eot?#iefix") format("embedded-opentype"), url("http://cdn.greeting.wedfairy.com/assets/fonts/qkbysjt-full-webfont.woff") format("woff"), url("http://cdn.greeting.wedfairy.com/assets/fonts/qkbysjt-full-webfont.ttf") format("truetype"), url("http://cdn.greeting.wedfairy.com/assets/fonts/qkbysjt-full-webfont.svg#FZQKBYSJW--GB1-0") format("svg") }';
+    var onload = _.after(2, function() {
+        _.defer(function() {
+            $('head').append($('<style></style>').text(style));
+        });
+    });
+    $.ajax({
+        url: 'http://cdn.greeting.wedfairy.com/assets/fonts/qkbysjt-full-webfont.ttf',
+        global: false, success: onload
+    });
+    $.ajax({
+        url: 'http://cdn.greeting.wedfairy.com/assets/fonts/qkbysjt-full-webfont.woff',
+        global: false, success: onload
+    });
+}
