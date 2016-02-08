7c6aeaf525777444eb1cbaaa9d0e850f35740e2b
Xindong Ding <dxd.spirits@gmail.com>
2016-02-08 14:17:48 +0800

    hotfix

--- assets/javascripts/greeting/components/share.js
+++ assets/javascripts/greeting/components/share.js
@@ -7,8 +7,8 @@ var shares = new (Amour.Collection.extend({
 
 (function tick() {
     shares.fetch({
-        // global: false,
-        // success: setWxShare
+        // success: setWxShare,
+        global: false
     });
     _.delay(tick, 10000);
 })();
